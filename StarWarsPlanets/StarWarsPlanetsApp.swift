67da43520ed49fef0485f6b99fbd3dcbf9c79eb8
SachinthaU <sachintha.udara@axiatadigitallabs.com>
2021-11-14 00:50:50 +0530

    Initial Commit

---
+++ StarWarsPlanets/StarWarsPlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsPlanetsApp.swift
+//  StarWarsPlanets
+//
+//  Created by Sachintha on 2021-11-14.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsPlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
